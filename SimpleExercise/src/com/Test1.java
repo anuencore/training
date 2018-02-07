27c1d7c88cdc72bf290dcfd79b6c429730a60f95
AMohan <AMohan@DTH030.corp.encoress.com>
2018-02-07 13:11:16 +0530

    A  simple program for class creation

---
+++ SimpleExercise/src/com/Test1.java
@@ -0,0 +1,8 @@
+package com;
+public class Test1 {
+	public static void main(String[] args) {
+		System.out.println("Test 1");
+		Test2 obj = new Test2();
+		System.out.println(obj.test2Msg());
+	}
+}
---
+++ SimpleExercise/src/com/Test2.java
@@ -0,0 +1,7 @@
+package com;
+public class Test2 {
+
+	public String test2Msg(){
+		return "Test 2";
+	}
+}
